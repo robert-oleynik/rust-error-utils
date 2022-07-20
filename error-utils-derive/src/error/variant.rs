e52720f0fc144236882eb02564e34aa2fe0717e2
Robert Oleynik <robert-oleynik@t-online.de>
2022-07-20 12:19:50 +0200

    Parse error variant

--- error-utils-derive/src/error/variant.rs
+++ error-utils-derive/src/error/variant.rs
@@ -2,10 +2,53 @@
 //
 // Copyright (c) 2022 Robert Oleynik
 
-pub struct Variant {}
+use syn::*;
+
+pub struct Variant {
+	impl_from: bool,
+	message: Option<LitStr>,
+	ident: Ident,
+	fields: Fields,
+}
 
 impl From<syn::Variant> for Variant {
-	fn from(_: syn::Variant) -> Self {
-		todo!()
+	fn from(variant: syn::Variant) -> Self {
+		let mut impl_from = false;
+		let mut message = None;
+		for option in variant
+			.attrs
+			.into_iter()
+			.map(|attr| attr.parse_meta().unwrap())
+			.filter_map(|meta| match meta {
+				Meta::List(list) => Some(list),
+				_ => None,
+			})
+			.filter(|meta| meta.path.is_ident("error"))
+			.flat_map(|meta| meta.nested.into_iter())
+		{
+			match option {
+				NestedMeta::Lit(Lit::Str(lit)) => {
+					if let Some(_) = &message {
+						panic!("Message already set for variant {}", variant.ident)
+					} else {
+						message = Some(lit)
+					}
+				},
+				NestedMeta::Meta(Meta::Path(path)) if path.is_ident("from") => {
+					if impl_from {
+						panic!("Option `from` already set for variant {}", variant.ident)
+					} else {
+						impl_from = true
+					}
+				},
+				_ => panic!("Unexpected option {:?} for variant {}", option, variant.ident),
+			}
+		}
+		Self {
+			impl_from,
+			message,
+			ident: variant.ident,
+			fields: variant.fields,
+		}
 	}
 }
